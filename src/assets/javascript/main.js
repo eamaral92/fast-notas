4159032e40ff91c0257042291b14def44586f1be
Erick <erick@erick-pc>
2017-07-16 15:35:14 -0300

    second

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,6 +1,6 @@
 // Header Effect
 $(window).bind('scroll', function () {
-  if ($(window).scrollTop() > 100) {
+  if ($(window).scrollTop() > 10) {
     $('#header-container').addClass('header-bg');
   } else {
     $('#header-container').removeClass('header-bg');
@@ -8,4 +8,11 @@ $(window).bind('scroll', function () {
 });
 
 // Slick
-$('.carrousel').slick();
+$('.carrousel').slick({
+	arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  prevArrow:"<button type='button' class='slick-prev'> < </button>",
+  nextArrow:"<button type='button' class='slick-next'> > </button>"
+});
