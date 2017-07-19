fe6cdc25a21841e5c44a3836d4307b476c0827ad
Erick <macweb01@MacWeb01.local>
2017-07-19 15:20:07 -0300

    finish forms

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -16,8 +16,3 @@ $('.carrousel').slick({
   prevArrow:"<button type='button' class='slick-prev'> < </button>",
   nextArrow:"<button type='button' class='slick-next'> > </button>"
 });
-
-// Validator
-
-
-$("#cadastro").validate();
